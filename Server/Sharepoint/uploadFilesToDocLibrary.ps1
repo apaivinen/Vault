d104d9824393b2392f29b3dc8ea08903d31c3385
Anssi Päivinen <apaivinen@gmail.com>
2017-10-02 12:15:27 +0300

    Add files via upload

---
+++ Server/Sharepoint/uploadFilesToDocLibrary.ps1
@@ -0,0 +1,28 @@
+$webUrl = "http://www.sivusto.epic/test/"
+
+$docLibraryName = "Kuvapankki"
+$docLibraryUrlName = "/test/kuvapankki/"
+$localFolderPath = "C:\temp\kirjasto"
+
+#Open web and library
+$web = Get-SPWeb $webUrl
+$docLibrary = $web.Lists[$docLibraryName]
+$files = ([System.IO.DirectoryInfo] (Get-Item $localFolderPath)).GetFiles()
+ForEach($file in $files)
+{
+	#Open file
+	$fileStream = ([System.IO.FileInfo] (Get-Item $file.FullName)).OpenRead()
+
+	#Add file
+	$folder =  $web.getfolder($docLibraryUrlName)
+
+	write-host "Copying file " $file.Name " to " $folder.ServerRelativeUrl "..."
+	$spFile = $folder.Files.Add($folder.Url + "/" + $file.Name, [System.IO.Stream]$fileStream, $true)
+	write-host "Success"
+
+	#Close filestream
+	$fileStream.Close();
+}
+
+
+$web.Dispose()
